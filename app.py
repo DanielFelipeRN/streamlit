c17a40d77b7f875a46fa60fc9b664352b3aeb48d
Daniel Felipe <82670885+DanielFelipeRN@users.noreply.github.com>
2026-08-24 16:08:38 -0500

    Fix typo in variable name from 'آہustar' to 'ajustar'

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown("""
             justify-content: flex-end;
         }
     </style>
-""", آہustar = True)
+""", ajustar = True)
 
 # Título compacto superior
 st.markdown("### 🏢 Control Automatizado de Acceso - Recepción Financiera")
